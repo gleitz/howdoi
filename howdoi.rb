e59bd068fd9672a2de23bc981b37fb07664f1fff
Benjamin Gleitzman <gleitz@mit.edu>
2012-12-12 00:02:20 -0800

    added homebrew package

---
+++ howdoi.rb
@@ -0,0 +1,12 @@
+require 'formula'
+
+class Howdoi < Formula
+  homepage 'https://github.com/gleitz/howdoi/'
+  url 'http://pypi.python.org/packages/source/h/howdoi/howdoi-0.1.2.tar.gz'
+  sha1 '26516710ec7b1d0f6b30527b3da424f8b7ba88e8'
+
+  def install
+    setup_args = ['setup.py', 'install']
+    system "/usr/bin/env", "python", *setup_args
+  end
+end
