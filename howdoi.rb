04895e8d19dca426bf389a5558d2de9a414d1f0f
Benjamin Gleitzman <gleitz@mit.edu>
2012-12-15 14:02:23 -0800

    update homebrew package

--- howdoi.rb
+++ howdoi.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Howdoi < Formula
   homepage 'https://github.com/gleitz/howdoi/'
-  url 'http://pypi.python.org/packages/source/h/howdoi/howdoi-0.1.3.tar.gz'
-  sha1 '26516710ec7b1d0f6b30527b3da424f8b7ba88e8'
+  url 'http://pypi.python.org/packages/source/h/howdoi/howdoi-0.2.tar.gz'
+  sha1 '69feff9525279641ccbf94995c0a047c5775eac7'
 
   def install
     setup_args = ['setup.py', 'install']
