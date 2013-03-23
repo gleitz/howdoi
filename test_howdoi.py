e650e90df48bbdcf6e38a0145889a968442a77c6
Benjamin Gleitzman <gleitz@mit.edu>
2013-03-23 09:40:22 -0600

    Added test for unicode error in Python 2.x

--- test_howdoi.py
+++ test_howdoi.py
@@ -64,6 +64,7 @@ class HowdoiTestCase(unittest.TestCase):
 
     def test_unicode_answer(self):
         assert self.call_howdoi('make a log scale d3')
+        assert self.call_howdoi('python unittest -n3')
 
 if __name__ == '__main__':
     unittest.main()
