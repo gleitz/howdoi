3ddeb24c628dbe7e291ce59849347b701be06006
Benjamin Gleitzman <gleitz@mit.edu>
2016-04-25 12:06:40 +0900

    Fix Homebrew download link

--- howdoi.rb
+++ howdoi.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Howdoi < Formula
   homepage 'https://github.com/gleitz/howdoi/'
-  url 'http://pypi.python.org/packages/source/h/howdoi/howdoi-1.1.9.tar.gz'
+  url 'https://pypi.python.org/packages/23/2d/9790707eed08c802daee32183e7c98ec2e9797564dad229738b7f178e18e/howdoi-1.1.9.tar.gz'
   sha1 '58d56ca5e652e20035b4869e589539feea5032aa'
 
   def install
