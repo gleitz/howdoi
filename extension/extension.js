d72f67abf0770e330d4306cc1a117e32c093fac0
Diana Arreola <arreoladiana11@gmail.com>
2020-06-10 18:28:37 -0700

    user can highlight word, run howdoi, and answer is updated in code editor

--- extension/extension.js
+++ extension/extension.js
@@ -1,6 +1,11 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const { spawn } = require("child_process");
+// const spawn = require('await-spawn');
+
+// const fs = require("fs");
+// const path = require("path");
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -10,18 +15,73 @@ const vscode = require('vscode');
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "howdoi" is now active!');
+	async function spawnChild(command, cb) {
+		console.log('2');
+		const process = await spawn("howdoi", [command]);
+		let result = []
+
+		process.stdout.on("data", data => {
+			console.log(`stdout: ${data}`);
+			result.push(String(data));
+		});
+
+		process.stderr.on("data", data => {
+			console.log(`stderr: ${data}`);
+		});
+		
+		process.on('error', (error) => {
+			console.log(`error: ${error.message}`);
+		});
+		
+		process.on("close", code => {
+			console.log(`child process exited with code ${code}`);
+			cb(result);
+		});
+		
+	}
+
+	function helperFunc(editor, myArr) {
+		console.log('1');
+
+		const quickPick = vscode.window.createQuickPick();
+			quickPick.items = myArr.map(x => ({label: x}));
+			quickPick.onDidChangeSelection(([item]) => {
+				if (item) {
+				// vscode.window.showInformationMessage(item.label);
+				editor.edit(edit => {
+					edit.replace(editor.selection, item.label);
+				});
+				quickPick.dispose();
+				}
+			});
+			quickPick.onDidHide(() => quickPick.dispose());
+			quickPick.show();			
+
+	}
+
+
+	
+
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('howdoi.helloWorld', function () {
+	let disposable = vscode.commands.registerCommand('howdoi.extension', function () {
 		// The code you place here will be executed every time your command is executed
+		console.log('1');
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			vscode.window.showInformationMessage('create a file to enable howdoi');
+			return;
+		}
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from howdoi!');
+		const text = editor.document.getText(editor.selection);
+		
+		spawnChild(text, function(myArr) {
+			helperFunc(editor, myArr);	
+		});
+
+
 	});
 
 	context.subscriptions.push(disposable);
