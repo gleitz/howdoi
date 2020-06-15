be17b687782038267ca40c6c51dd432485e42811
Diana Arreola <arreoladiana11@gmail.com>
2020-06-15 11:29:29 -0700

    users can now type usage: howdoi [-h] [-p POS] [-a] [-l] [-c] [-n NUM_ANSWERS] [-C] [-v]               [-e [SEARCH_ENGINE]]               [QUERY [QUERY ...]]
    instant coding answers via the command line
    
    positional arguments:
      QUERY                 the question to answer
    
    optional arguments:
      -h, --help            show this help message and exit
      -p POS, --pos POS     select answer in specified position (default: 1)
      -a, --all             display the full text of the answer
      -l, --link            display only the answer link
      -c, --color           enable colorized output
      -n NUM_ANSWERS, --num-answers NUM_ANSWERS
                            number of answers to return
      -C, --clear-cache     clear the cache
      -v, --version         displays the current version of howdoi
      -e [SEARCH_ENGINE], --engine [SEARCH_ENGINE]
                            change search engine for this query only+question in code editor

--- extension/extension.js
+++ extension/extension.js
@@ -2,8 +2,8 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 const { spawn } = require("child_process");
-const { create } = require('domain');
-const { textChangeRangeNewSpan } = require('typescript');
+// const { create } = require('domain');
+// const { textChangeRangeNewSpan } = require('typescript');
 const path = require("path");
 
 // this method is called when your extension is activated
@@ -15,8 +15,9 @@ const path = require("path");
 function activate(context) {
 
 	async function spawnChild(command, cb) {
-		const process = await spawn("howdoi", [command, '-n 3']);
-		let result = []
+		const updatedCommand = howdoiPrefix(command);
+		const process = await spawn("howdoi", [updatedCommand, '-n 3']);
+		let result = [];
 		process.stdout.on("data", data => {
 
 			result.push(String(data));
@@ -69,54 +70,70 @@ function activate(context) {
 			quickPick.onDidHide(() => quickPick.dispose());
 			quickPick.show();			
 	}
-	function modifyCommentedText(textToBeModified){
+
+	function howdoiPrefix(command) {
+		const prefix = "howdoi";
+		
+		if (command.includes(prefix)) {
+			const newCommand = command.replace(prefix,'');
+			console.log('prefix', newCommand);
+			return newCommand;
+		}
+		else {
+			console.log('NOprefix');
+			return command
+		}
+
+	}
+
+	function modifyCommentedText(textToBeModified) {
 		var regexBegins =  /^[!@#<>/\$%\^\&*\)\(+=._-]+/
 		var regexEnds = /[!@#<>/\$%\^\&*\)\(+=._-]+$/
 			
 			
 			if (textToBeModified.match(regexBegins) && textToBeModified.match(regexEnds)){
-				textToBeModified = textToBeModified.replace(regexBegins, '')
-				textToBeModified = textToBeModified.replace(regexEnds, '')
+				textToBeModified = textToBeModified.replace(regexBegins, '');
+				textToBeModified = textToBeModified.replace(regexEnds, '');
 			}
 			else if(textToBeModified.match(regexEnds)){
-				textToBeModified = textToBeModified.replace(regexEnds, '')
+				textToBeModified = textToBeModified.replace(regexEnds, '');
 			}
 			else if(textToBeModified.match(regexBegins)){
-				textToBeModified = textToBeModified.replace(regexBegins, '')
+				textToBeModified = textToBeModified.replace(regexBegins, '');
 			}
 		
-		return textToBeModified
+		return textToBeModified;
 	}
-	function determineCommentStyle(currentlyOpenTabfileName){
-	   var fileType = currentlyOpenTabfileName.substring(currentlyOpenTabfileName.lastIndexOf('.')+1, currentlyOpenTabfileName.length) || currentlyOpenTabfileName;
+	// function determineCommentStyle(currentlyOpenTabfileName){
+	//    var fileType = currentlyOpenTabfileName.substring(currentlyOpenTabfileName.lastIndexOf('.')+1, currentlyOpenTabfileName.length) || currentlyOpenTabfileName;
 	 
-	   console.log("This is the filetype" + fileType)
+	//    console.log("This is the filetype" + fileType);
 	 
-	   var commentStyleTop = " "
-	   if(fileType.equal("py")){
-		  commentStyleTop = " ''' "
-	   }
-	   else if(fileType.equal("java")){
-		   commentStyleTop = "/*"
-	   }
-	   else if(fileType.equal("txt")){
-		   commentStyleTop = " "
-	   }
-	   console.log("none of it" + commentStyleTop)
-		return fileType
-		//will/should return the comment style instead but using filetype as a placeholder for now
-	}
+	//    var commentStyleTop = " ";
+	//    if(fileType.equal("py")){
+	// 	  commentStyleTop = " ''' ";
+	//    }
+	//    else if(fileType.equal("java")){
+	// 	   commentStyleTop = "/*";
+	//    }
+	//    else if(fileType.equal("txt")){
+	// 	   commentStyleTop = " ";
+	//    }
+	//    console.log("none of it" + commentStyleTop);
+	// 	return fileType;
+	// 	//will/should return the comment style instead but using filetype as a placeholder for now
+	// }
 
 
 	function getFileName(){
 		var currentlyOpenTabfilePath = vscode.window.activeTextEditor.document.uri.fsPath;
 		var currentlyOpenTabfileName = path.basename(currentlyOpenTabfilePath);
-		console.log( "path"+ currentlyOpenTabfilePath)
-		console.log("name that will be passed to determine commentstyle" + currentlyOpenTabfileName)
+		console.log( "path"+ currentlyOpenTabfilePath);
+		console.log("name that will be passed to determine commentstyle" + currentlyOpenTabfileName);
 		
 		//var style = determineCommentStyle(currentlyOpenTabfileName)
 
-	   return currentlyOpenTabfileName
+	   return currentlyOpenTabfileName;
 
 	}
 
@@ -131,16 +148,17 @@ function activate(context) {
 			vscode.window.showInformationMessage('create a file to enable howdoi');
 			return;
 		}
-		
-		var nameOfFile = getFileName()
-		var commentStyle = determineCommentStyle(nameOfFile)
-		
+
+		const nameOfFile = getFileName();
+		console.log('nameoffile:', nameOfFile);
+		// const commentStyle = determineCommentStyle(nameOfFile);
+		// console.log('commentstyle:', commentStyle);
 		//and then pass it to the async function alongside the highlighted text so that it can be used to 
 		//output something correctly
 		
-		var textToBeModified = editor.document.getText(editor.selection);
-		var textToBeSearched = modifyCommentedText(textToBeModified)
-	
+		const textToBeModified = editor.document.getText(editor.selection);
+		const textToBeSearched = modifyCommentedText(textToBeModified);
+
 		spawnChild(textToBeSearched, function(myArr) {
 			helperFunc(editor, myArr, textToBeSearched);	
 		});
