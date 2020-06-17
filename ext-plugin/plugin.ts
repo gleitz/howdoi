f211084f7ac08a128d5d7a8ac6cbbb7e73f7a2a5
Diana Arreola <arreoladiana11@gmail.com>
2020-06-17 12:42:47 -0700

    Created initial plugin files

---
+++ ext-plugin/plugin.ts
@@ -0,0 +1,106 @@
+"use strict";
+import * as cp from "child_process";
+
+main();
+function main() {
+    const txt:string = '\\ howdoi print python';
+    const txtArr:string[] = modifyCommentedText(txt);
+    const textToBeSearched:string = txtArr[0];
+    const commentBegin:string  = txtArr[1];
+    const commentEnd:string = txtArr[2];
+
+    spawnChild(textToBeSearched, function(myArr:string[]) {
+        helperFunc(myArr,txt, commentBegin, commentEnd);
+    });
+
+}
+
+//  process
+async function spawnChild(command:string, callback:any) {
+    const updatedCommand = howdoiPrefix(command);
+    const process = await cp.spawn("howdoi", [updatedCommand, '-n 3']);
+    let result:string[] = [];
+    process.stdout.on("data", data => {
+        result.push(String(data));
+    });
+
+    process.stderr.on("data", data => {
+        console.log(`stderr: ${data}`);
+    });
+    
+    process.on('error', (error) => {
+        console.log(`error: ${error.message}`);
+    });
+    
+    process.on("close", code => {
+        console.log(`child process exited with code ${code}`);
+        callback(result);
+    });
+}
+
+
+// removes howdoi from command
+function howdoiPrefix(command:string) {
+    const prefix = "howdoi";
+    
+    if (command.includes(prefix)) {
+        const newCommand = command.replace(prefix,'');
+        return newCommand;
+    }
+    else {
+        return command;
+    }
+}
+
+//  only for single line comments
+function modifyCommentedText(textToBeModified:string) {
+    const regexBegins:RegExp =  /^[!@#<>/\$%\^\&*\)\(+=._-]+/;
+    const regexEnds:RegExp = /[!@#<>/\$%\^\&*\)\(+=._-]+$/;
+    let commentBegin:string;
+    let commentEnd:string;	
+        
+    if (textToBeModified.match(regexBegins) && textToBeModified.match(regexEnds)){
+        commentBegin = textToBeModified.match(regexBegins)!.join();
+        commentEnd = textToBeModified.match(regexEnds)!.join();
+        textToBeModified = textToBeModified.replace(regexBegins, '');
+        textToBeModified = textToBeModified.replace(regexEnds, '');
+        let result:string[]  = [textToBeModified, commentBegin, commentEnd];
+        return result;
+    }
+    else if(textToBeModified.match(regexEnds)){
+        commentEnd = textToBeModified.match(regexEnds)!.join();
+        textToBeModified = textToBeModified.replace(regexEnds, '');
+        let result:string[] = [textToBeModified,'',commentEnd];
+        return result;
+    }
+    else if(textToBeModified.match(regexBegins)){
+        commentBegin = textToBeModified.match(regexBegins)!.join();
+        textToBeModified = textToBeModified.replace(regexBegins, '');
+        let result:string[] = [textToBeModified, commentBegin, ''];
+        return result;
+    }
+    else {
+        let result:string[] = [textToBeModified, '', ''];
+        return result;
+    }
+
+}
+function spliceArr(obj:string[], commentBegin:string, commentEnd:string) {
+    let dataString = String(obj);
+    let lines = dataString.split('\n'+'================================================================================' + '\n' + '\n');
+    let newArr:string[][] = lines.map((elem) => elem.split(' ★'));
+    for (let i = 0; i < newArr.length; i++) {
+        newArr[i][0] = commentBegin + newArr[i][0] + commentEnd;
+    }
+    return newArr;
+}
+
+function helperFunc(resultArr,userTxt, commentBegin, commentEnd) {
+    console.log('in helperfunc');
+    const newResult = spliceArr(resultArr,commentBegin,commentEnd);
+    console.log('result', newResult);
+
+}
+
+    
+
