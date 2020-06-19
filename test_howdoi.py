c183f79f34e400fd0741bba53b15e72f5acc7bd8
Christopher Nguyen <chrisngyn99@gmail.com>
2020-06-19 16:19:00 -0400

    TESTS NOW WORKING

--- test_howdoi.py
+++ test_howdoi.py
@@ -28,7 +28,6 @@ class HowdoiTestCase(unittest.TestCase):
                             'mel']
 
     def assertValidResponse(self, res):
-        print(res)
         self.assertTrue(len(res) > 0)
 
     def tearDown(self):
@@ -50,10 +49,7 @@ class HowdoiTestCase(unittest.TestCase):
         for query in self.queries:
             self.assertValidResponse(self.call_howdoi(query))
         for query in self.bad_queries:
-            print(query)
-            print("BEGIN ============================")
             self.assertValidResponse(self.call_howdoi(query))
-            print("END   ============================")
 
         os.environ['HOWDOI_URL'] = 'pt.stackoverflow.com'
         for query in self.pt_queries:
@@ -64,10 +60,7 @@ class HowdoiTestCase(unittest.TestCase):
         for query in self.queries:
             self.assertValidResponse(self.call_howdoi(query))
         for query in self.bad_queries:
-            print(query)
-            print("BEGIN ============================")
             self.assertValidResponse(self.call_howdoi(query))
-            print("END   ============================")
 
         os.environ['HOWDOI_URL'] = 'pt.stackoverflow.com'
         for query in self.pt_queries:
@@ -80,10 +73,7 @@ class HowdoiTestCase(unittest.TestCase):
         for query in self.queries:
             self.assertValidResponse(self.call_howdoi(query))
         for query in self.bad_queries:
-            print(query)
-            print("BEGIN ============================")
             self.assertValidResponse(self.call_howdoi(query))
-            print("END   ============================")
 
         os.environ['HOWDOI_URL'] = 'pt.stackoverflow.com'
         for query in self.pt_queries:
