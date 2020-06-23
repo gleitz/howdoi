f1bd14077096ddb8a368cdee9f37c094d5ec2c17
Cesare De Cal <hi@cesare.io>
2020-06-23 15:11:48 +0200

    Refactored logic to get authors in separate function

--- howdoi/arxiv.py
+++ howdoi/arxiv.py
@@ -23,34 +23,35 @@ def query_page(search_query, start=0, max_results=1):
 
 def parse(response):
     root = ET.fromstring(response)
-    for entry in root.findall("atom:entry", NS):
+    for entry in root.findall("atom:entry", NAMESPACES):
         # If there are no results, arXiv sometimes returns just a blank entry
-        if entry.find("atom:id", NS) is None:
+        if entry.find("atom:id", NAMESPACES) is None:
             continue
         return convert_entry_to_paper(entry)
 
 
-def convert_entry_to_paper(entry):
-    paper_details = {}
-    paper_details['arxiv_id'] = entry.find("atom:id", NS).text.split('/')[-1]
-    paper_details['title'] = entry.find("atom:title", NS).text
-    paper_details['title'] = paper_details['title'].replace('\n', '').replace('  ', ' ')
-    paper_details['summary'] = entry.find('atom:summary', NS).text
-    paper_details['authors'] = []
-    for author in entry.findall('atom:author', NS):
-        paper_details['authors'].append({
-            'name': author.find('atom:name', NS).text,
-            'affiliation': [e.text for e in author.findall('arxiv:affiliation', NS)],
+def get_authors(entry):
+    authors = []
+    for author in entry.findall('atom:author', NAMESPACES):
+        authors.append({
+            'name': author.find('atom:name', NAMESPACES).text,
+            'affiliation': [e.text for e in author.findall('arxiv:affiliation', NAMESPACES)],
         })
-    paper_details['arxiv_url'] = entry.find(
-        "./atom:link[@type='text/html']", NS).attrib['href']
-    paper_details['pdf_url'] = entry.find(
-        "./atom:link[@type='application/pdf']", NS).attrib['href']
-    paper_details['primary_category'] = entry.find(
-        'arxiv:primary_category', NS).attrib['term']
-    paper_details['categories'] = [e.attrib['term'] for e in entry.findall('atom:category', NS)]
-    # Optional
-    paper_details['comment'] = getattr(entry.find('arxiv:comment', NS), 'text', None)
-    paper_details['doi'] = getattr(entry.find('arxiv:doi', NS), 'text', None)
-    paper_details['journal_ref'] = getattr(entry.find('arxiv:journal_ref', NS), 'text', None)  
-    return paper_details
+    return authors
+
+
+def convert_entry_to_paper(entry):
+    paper = {}
+    paper['arxiv_id'] = entry.find("atom:id", NAMESPACES).text.split('/')[-1]
+    paper['title'] = entry.find("atom:title", NAMESPACES).text
+    paper['title'] = paper['title'].replace('\n', '').replace('  ', ' ')
+    paper['summary'] = entry.find('atom:summary', NAMESPACES).text
+    paper['authors'] = get_authors(entry)
+    paper['arxiv_url'] = entry.find("./atom:link[@type='text/html']", NAMESPACES).attrib['href']
+    paper['pdf_url'] = entry.find("./atom:link[@type='application/pdf']", NAMESPACES).attrib['href']
+    paper['primary_category'] = entry.find('arxiv:primary_category', NAMESPACES).attrib['term']
+    paper['categories'] = [e.attrib['term'] for e in entry.findall('atom:category', NAMESPACES)]
+    paper['comment'] = getattr(entry.find('arxiv:comment', NAMESPACES), 'text', None)
+    paper['doi'] = getattr(entry.find('arxiv:doi', NAMESPACES), 'text', None)
+    paper['journal_ref'] = getattr(entry.find('arxiv:journal_ref', NAMESPACES), 'text', None)
+    return paper
