691da317fd6727f7668c1e06650210b678895fe4
yk396 <yk396@cornell.edu>
2020-06-23 13:54:08 -0400

    Add get cache key function that appends version number to args string

--- howdoi/howdoi.py
+++ howdoi/howdoi.py
@@ -396,8 +396,8 @@ def _format_answers(res, args):
 
     splitter_length = 80
     answer_splitter = '\n' + '=' * splitter_length + '\n\n'
-
     formatted_answers = []
+    
     for answer in res:
         next_ans = answer["answer"]
         if args["link"]:  # if we only want links
@@ -422,6 +422,10 @@ def _get_help_instructions():
     return instruction_splitter.join(instructions)
 
 
+def _get_cache_key(args):
+    return str(args) + __version__
+
+
 def howdoi(raw_query):
     args = raw_query
     if type(raw_query) is str:  # you can pass either a raw or a parsed query
@@ -429,12 +433,13 @@ def howdoi(raw_query):
         args = vars(parser.parse_args(raw_query.split(' ')))
 
     args['query'] = ' '.join(args['query']).replace('?', '')
-    cache_key = str(args)
+    cache_key = _get_cache_key(args)
 
     if _is_help_query(args['query']):
         return _get_help_instructions() + '\n'
 
     res = cache.get(cache_key)
+
     if res:
         return _format_answers(res, args)
 
