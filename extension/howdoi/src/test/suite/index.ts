7c5ab1f32e9c05ec537ac2a00c1e5df7569f0679
Benjamin Gleitzman <gleitz@mit.edu>
2020-06-26 06:05:16 -0700

    A few readability suggestions
    - Add a space after variable and before typescript types (`foo:HowdoiResult` -> `foo:
    HowdoiResult`)
    - Remove all semicolons (It's weird, I know, but they aren't necessary
    in Typescript. I also changed the `.eslintrc` file to show errors if
    semicolons are included)
    - Prefer using `'` everywhere rather than `"` (added eslint rule)
    - Make sure that there is always a space after a comma (added eslint
    rule)
    
    Some of these changes are personal preference, but by attaching an
    associated eslint rule and requiring that linting pass before a PR can
    be accepted, you can keep the codebase readable and uniform.

--- extension/howdoi/src/test/suite/index.ts
+++ extension/howdoi/src/test/suite/index.ts
@@ -20,19 +20,19 @@ export function run(): Promise<void> {
       // Add files to the test suite
       files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
 
-	  try {
+      try {
         // Run the mocha test
         mocha.run(failures => {
           if (failures > 0) {
             e(new Error(`${failures} tests failed.`));
-            } else {
-              c();
-            }
+          } else {
+            c();
+          }
         });
       } catch (err) {
-		  console.error(err);
-		  e(err);
-        } 
+        console.error(err);
+        e(err);
+      }
     });
   });
 }
