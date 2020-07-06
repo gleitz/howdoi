b107fae91e48cb612dc9afb4e5566c7a41c9feb7
Diana Arreola <arreoladiana11@gmail.com>
2020-07-06 12:01:36 -0700

    changes to extension test file

--- extension/vscode-howdoi/src/test/suite/extension.test.ts
+++ extension/vscode-howdoi/src/test/suite/extension.test.ts
@@ -1,33 +1,60 @@
 import * as assert from 'assert'
 import * as vscode from 'vscode'
+import * as Mocha from 'mocha'
 import * as myExtension from '../../extension'
 
 suite('Extension Test Suite', () => {
   vscode.window.showInformationMessage('Start all tests.')
-  test('removeHowdoiPrefix test', () => {
-    assert.equal(myExtension.removeHowdoiPrefix('howdoi query'), 'query')
-    assert.equal(myExtension.removeHowdoiPrefix(' howdoi query '), 'query')
-    assert.equal(myExtension.removeHowdoiPrefix('query'), 'query')
-    assert.equal(myExtension.removeHowdoiPrefix(' query '), 'query')
+  suite('removeHowdoiPrefix test', () => {
+    test('test 1', function () {
+      assert.equal(myExtension.removeHowdoiPrefix('howdoi query'), 'query')
+    })
+    test('test 2', function () {
+      assert.equal(myExtension.removeHowdoiPrefix(' howdoi query '), 'query')
+    })
+    test('test 3', function () {
+      assert.equal(myExtension.removeHowdoiPrefix('query'), 'query')
+    })
+    test('test 4', function () {
+      assert.equal(myExtension.removeHowdoiPrefix(' query '), 'query')
+    })
+
   })
-  test('modifyCommentedText test', () => {
-    // null
-    assert.equal(myExtension.modifyCommentedText('howdoi query'), null)
-    // JS, TS, C/ C++/ C#, Java, GO, Rust, Scala, Swift, J#, Dlang single line comment
-    assert.deepEqual(myExtension.modifyCommentedText('// howdoi query'), ['howdoi query', '//', ''])
-    assert.deepEqual(myExtension.modifyCommentedText('//howdoi query'), ['howdoi query', '//', ''])
-    // Python, Ruby, powershell, Julia, R, prolog, Crystal, Dockerfile, Diff single line comment
-    assert.deepEqual(myExtension.modifyCommentedText('# howdoi query'), ['howdoi query', '#', ''] )
-    assert.deepEqual(myExtension.modifyCommentedText('#howdoi query'), ['howdoi query', '#', ''] )
-    // C++, CSS single line comment 
-    assert.deepEqual(myExtension.modifyCommentedText('/* howdoi query */'), ['howdoi query', '/*', '*/'] )
-    // HTML, PHP, Markdown, Vue single line comment
-    assert.deepEqual(myExtension.modifyCommentedText('<!-- howdoi query -->'), ['howdoi query', '<!--', '-->'] )
-    // SQL, Haskell single line comment
-    assert.deepEqual(myExtension.modifyCommentedText('-- howdoi query'), ['howdoi query', '--', ''] )
-    // LaTex single line comment
-    assert.deepEqual(myExtension.modifyCommentedText('% howdoi query'), ['howdoi query', '%', ''] )
-    // clojure single line comment
-    assert.deepEqual(myExtension.modifyCommentedText('; howdoi query'), ['howdoi query', ';', ''] ) 
+  suite('modifyCommentedText test', () => {
+
+    test('test 1', function () {
+      // null
+      assert.equal(myExtension.modifyCommentedText('howdoi query'), null)
+    })
+    test('test 2', function () {
+      // JS, TS, C/ C++/ C#, Java, GO, Rust, Scala, Swift, J#, Dlang single line comment
+      assert.deepEqual(myExtension.modifyCommentedText('// howdoi query'), ['howdoi query', '//', ''])
+      assert.deepEqual(myExtension.modifyCommentedText('//howdoi query'), ['howdoi query', '//', ''])
+    })
+    test('test 3', function () {
+      // Python, Ruby, powershell, Julia, R, prolog, Crystal, Dockerfile, Diff single line comment
+      assert.deepEqual(myExtension.modifyCommentedText('# howdoi query'), ['howdoi query', '#', ''] )
+      assert.deepEqual(myExtension.modifyCommentedText('#howdoi query'), ['howdoi query', '#', ''] )
+    })
+    test('test 4', function () {
+      // C++, CSS single line comment 
+      assert.deepEqual(myExtension.modifyCommentedText('/* howdoi query */'), ['howdoi query', '/*', '*/'] )
+    })
+    test('test 5', function () {
+      // HTML, PHP, Markdown, Vue single line comment
+      assert.deepEqual(myExtension.modifyCommentedText('<!-- howdoi query -->'), ['howdoi query', '<!--', '-->'] )
+    })
+    test('test 6', function () {
+      // SQL, Haskell single line comment
+      assert.deepEqual(myExtension.modifyCommentedText('-- howdoi query'), ['howdoi query', '--', ''] )
+    })
+    test('test 7', function () {
+      // LaTex single line comment
+      assert.deepEqual(myExtension.modifyCommentedText('% howdoi query'), ['howdoi query', '%', ''] )
+    })
+    test('test 8', function () {
+      // clojure single line comment
+      assert.deepEqual(myExtension.modifyCommentedText('; howdoi query'), ['howdoi query', ';', ''] ) 
+    }) 
   })
 })
