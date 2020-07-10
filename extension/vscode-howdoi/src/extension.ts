b4651b3236cb500b2f3e19b573b115f55350c397
Diana Arreola <arreoladiana11@gmail.com>
2020-07-09 18:47:06 -0700

    Enabled json on vscode extension

--- extension/vscode-howdoi/src/extension.ts
+++ extension/vscode-howdoi/src/extension.ts
@@ -3,130 +3,25 @@ import * as cp from 'child_process'
 
 const HOWDOI_PREFIX = 'howdoi'
 
-interface HowdoiResult {
+interface HowdoiObj {
     question: string
     answer: string[]
     link: string[]
 }
 
+interface JSONObj {
+  answer: string,
+  link: string,
+  position: string
+}
+
 interface CallBack {
-  (howdoiOutput: string): void
+  (howdoiOutput: JSONObj[]): void
 }
 
-export async function spawnChild(command: string, callbackFunc: CallBack): Promise<void> {
-  const commandWithoutPrefix = removeHowdoiPrefix(command)
-  const process = await cp.spawn(HOWDOI_PREFIX, [commandWithoutPrefix, '-n 3'])
-  let howdoiCommandOutput: string = ''
-
-  process.stdout.on('data', (data: Buffer) => {
-    howdoiCommandOutput += String(data)
-  })
-
-  process.stderr.on('data', (data: Buffer) => {
-    console.log(`stderr: ${data}`)
-  })
-
-  process.on('error', (error: Error) => {
-    console.log(`error: ${error.message}`)
-  })
-
-  process.on('close', (code: number) => {
-    console.log(`child process exited with code ${code}`)
-    return callbackFunc(howdoiCommandOutput)
-  })
-}
-
-export function removeHowdoiPrefix(command: string): string {
-  if (!command.trim().startsWith(HOWDOI_PREFIX)) {
-    return command.trim()
-  }
-  return command.replace(HOWDOI_PREFIX, '').trim()
-}
-
-export function modifyCommentedText(userCommand: string): string[]|null {
-  /* This function finds the comment regex, removes it from the string and returns an array
-  with the modified string, the beginning comment regex, ending comment regex */
-  const frontCommentRegex = /^[!@#<>/;%*(+=._-]+/
-  const endCommentRegex = /[!@#<>/%*+=._-]+$/
-  let frontCommentChar: string
-  let endCommentChar: string
-  let userCommandWithoutComment: string[]
-  const initialMatchRegex: RegExpMatchArray | null = userCommand.match(frontCommentRegex)
-  const endMatchRegex: RegExpMatchArray | null = userCommand.match(endCommentRegex)
-
-  if (initialMatchRegex && endMatchRegex){
-    frontCommentChar = initialMatchRegex.join()
-    endCommentChar = endMatchRegex.join()
-    userCommand = userCommand.replace(frontCommentRegex, '')
-    userCommand = userCommand.replace(endCommentRegex, '')
-    userCommandWithoutComment = [userCommand.trim(), frontCommentChar, endCommentChar]
-    return userCommandWithoutComment
-  }
-  else if(endMatchRegex){
-    endCommentChar = endMatchRegex.join()
-    userCommand = userCommand.replace(endCommentRegex, '')
-    userCommandWithoutComment = [userCommand.trim(), '', endCommentChar]
-    return userCommandWithoutComment
-  }
-  else if(initialMatchRegex){
-    frontCommentChar = initialMatchRegex.join()
-    userCommand= userCommand.replace(frontCommentRegex, '')
-    userCommandWithoutComment = [userCommand.trim(), frontCommentChar, '']
-    return userCommandWithoutComment
-  }
-  else {
-    return null
-  }
-}
-
-function organizeHowdoiOutput(howdoiOutput: string, frontCommentChar: string, endCommentChar: string): string[][] {
-  /* Creates an array from the howdoiOutput string in which each element
-  is one of three answers from the usersCommand */
-  const delim = '\n'+'================================================================================'+'\n'+'\n'
-  const howdoiAnswersArr: string[] = howdoiOutput.split(delim)
-  /* Creates a 2D array from howdoiAnswersArr in which each element is an array which denotes
-  one of three answers from the usersCommand, and the elements in that array are the link and answer */
-  let newHowdoiAnswersArr: string[][] = howdoiAnswersArr.map((elem) => elem.split(' ★'))
-  // The comment Regex is added to the link string
-  for (let i = 0; i < newHowdoiAnswersArr.length; i++) {
-    newHowdoiAnswersArr[i][0] = frontCommentChar + newHowdoiAnswersArr[i][0] + endCommentChar
-  }
-  return newHowdoiAnswersArr
-}
-
-export function createHowdoiResult(howdoiResultArr: string[][], userCommand: string): HowdoiResult {
-  let howdoiResultObj: HowdoiResult = {question: userCommand, answer: [], link: []}
-
-  for (let i = 0; i < howdoiResultArr.length; i++) {
-    howdoiResultObj.link.push(howdoiResultArr[i][0])
-    howdoiResultObj.answer.push(howdoiResultArr[i][1])
-  }
-
-  return howdoiResultObj
-}
-
-export function howdoi(howdoiOutput: string, userCommand: string, frontCommentChar: string, endCommentChar: string): HowdoiResult {
-  const organizedHowdoiArr: string[][] = organizeHowdoiOutput(howdoiOutput, frontCommentChar, endCommentChar)
-  const howdoiResultObj: HowdoiResult = createHowdoiResult(organizedHowdoiArr, userCommand)
-  return howdoiResultObj
-}
-
-export function quickPicker(editor: any, howdoiResultObj: HowdoiResult, userCommand: string): void {
-  const quickPick = vscode.window.createQuickPick()
-
-  quickPick.items = howdoiResultObj.answer.map((answer: any) => (
-    {label: answer, link: howdoiResultObj.link[howdoiResultObj.answer.indexOf(answer)] }))
-
-  quickPick.onDidChangeSelection(([item]: any) => {
-    if (item) {
-      editor.edit((edit: any) => {
-        edit.replace(editor.selection, userCommand + '\n' + item.link + item.label)
-      })
-	  quickPick.dispose()
-    }
-  })
-  quickPick.onDidHide(() => quickPick.dispose())
-  quickPick.show()
+interface CommentChars {
+  frontComment: string,
+  endComment: string
 }
 
 export function activate(context: vscode.ExtensionContext) {
@@ -140,27 +35,160 @@ export function activate(context: vscode.ExtensionContext) {
     }
 
     const userCommand: string = editor.document.getText(editor.selection)
-    const userCommandWithoutComment: string[]|null = modifyCommentedText(userCommand)
+    
+    // retrieve single line comment regexes
+    const commentChar: CommentChars|null = findCommentChar(userCommand)
 
-    if (userCommandWithoutComment !== null) {
-      const textToBeSearched: string = userCommandWithoutComment[0]
-      const frontCommentChar: string  = userCommandWithoutComment[1]
-      const endCommentChar: string = userCommandWithoutComment[2]
+    if (commentChar !== null) {
+      // comment removed from usercommand
+      const commandWithoutComment: string = removeCommentChar(userCommand, commentChar)
 
-      const callbackFunc: CallBack = function(howdoiOutput: string): void {
-        
-        let howdoiResultObj = howdoi(howdoiOutput, userCommand, frontCommentChar, endCommentChar)
+      // create callback function
+      const callbackFunc: CallBack = function(howdoiJSON: JSONObj[]): void{
+        let howdoiResultObj = createHowdoiObj(howdoiJSON, userCommand, commentChar)
+        console.log('howdoiResultobj: ', howdoiResultObj)
         quickPicker(editor, howdoiResultObj, userCommand)
-      
       }
 
-      spawnChild(textToBeSearched, callbackFunc)
+      retrieveHowdoiOutput(commandWithoutComment, callbackFunc)
     }
     else {
       vscode.window.showErrorMessage('please use single line comment for howdoi.')
     }
   })
   context.subscriptions.push(disposable)
+}
+
+export function findCommentChar(userCommand: string): CommentChars|null { 
+  /* This function finds the comment regex, removes it from the string and returns a
+   CommentChars interface with the beginning comment regex and ending comment regex */
+  const frontCommentRegex =  /^[!@#<>/;%*(+=._-]+/
+  const endCommentRegex = /[!@#<>/%*+=._-]+$/
+  let frontCommentChar: string
+  let endCommentChar: string
+  let userCommandWithoutComment: CommentChars
+  const initialMatchRegex: RegExpMatchArray | null = userCommand.match(frontCommentRegex)
+  const endMatchRegex: RegExpMatchArray | null = userCommand.match(endCommentRegex)
+        
+  if (initialMatchRegex && endMatchRegex){
+    frontCommentChar = initialMatchRegex.join()
+    endCommentChar = endMatchRegex.join()
+    userCommandWithoutComment = {
+      frontComment: frontCommentChar,
+      endComment: endCommentChar
+    }
+    return userCommandWithoutComment
+  }
+  else if(initialMatchRegex){
+    frontCommentChar = initialMatchRegex.join()
+    userCommandWithoutComment = {
+      frontComment: frontCommentChar,
+      endComment: ''
+    }
+    return userCommandWithoutComment
+  }
+  else {
+    return null
+  }
+}
+
+export function removeCommentChar(userCommand: string, commentChar: CommentChars): string {
+  // removes single line comment regex from the user command
+  const frontCommentChar: string = commentChar.frontComment
+  const endCommentChar: string = commentChar.endComment
+
+  if (!userCommand.includes(endCommentChar)) {
+    return userCommand.replace(frontCommentChar, '').trim()
+  }
+  userCommand = userCommand.replace(frontCommentChar, '')
+  return userCommand.replace(endCommentChar, '').trim()
+}
+
+export async function retrieveHowdoiOutput(command: string, callbackFunc: CallBack): Promise<void> {
+  console.log('in spawn')
+  const commandWithoutPrefix = removeHowdoiPrefix(command)
+  // HOWDOI_PREFIX, [commandWithoutPrefix, '-n3']
+  const process = cp.spawn(HOWDOI_PREFIX, [commandWithoutPrefix, '-n3', '-j'])
+  console.log('process', process)
+  let howdoiJSON: JSONObj[]
+
+  process.stdout.on('data', (data: string) => {
+    howdoiJSON = JSON.parse(data)
+  })
+
+  process.stderr.on('data', (data: Buffer) => {
+    console.log(`stderr: ${data}`)
+  })
+
+  process.on('error', (error: Error) => {
+    console.log(`error: ${error.message}`)
+  })
+
+  process.on('close', (code: number) => {
+    console.log(`child process exited with code ${code}`)
+    return callbackFunc(howdoiJSON)
+  })
+}
+
+export function removeHowdoiPrefix(command: string): string {
+  if (!command.trim().startsWith(HOWDOI_PREFIX)) {
+    return command.trim()
+  }
+  return command.replace(HOWDOI_PREFIX, '').trim()
+}
+
+export function createHowdoiObj(parsedJson: JSONObj[], userCommand: string, commentChar: CommentChars): HowdoiObj {
+  // creates a HowdoiObj interface 
+  const howdoiObj: HowdoiObj = {question: userCommand, answer: [], link: []}
+
+  for (let i = 0; i < parsedJson.length; i++) {
+    howdoiObj.answer.push(parsedJson[i].answer.trim())
+    howdoiObj.link.push(addComment(parsedJson[i].link.trim(), commentChar))
+  }
+
+  return howdoiObj
+}
+
+export function createHowdoiResult(howdoiResultArr: string[][], userCommand: string): HowdoiObj {
+  let howdoiResultObj: HowdoiObj = {question: userCommand, answer: [], link: []}
+
+  for (let i = 0; i < howdoiResultArr.length; i++) {
+    howdoiResultObj.link.push(howdoiResultArr[i][0])
+    howdoiResultObj.answer.push(howdoiResultArr[i][1])
+  }
+
+  return howdoiResultObj
+}
+
+export function addComment(command: string, commentChar: CommentChars): string {
+  // adds single line comment to string provided
+  const frontCommentChar: string = commentChar.frontComment
+  const endCommentChar: string = commentChar.endComment
+  if (frontCommentChar && (endCommentChar !== '')) {
+    const commentedCommand: string = frontCommentChar + ' ' + command + ' ' + endCommentChar
+    return commentedCommand
+  }
+  const commentedCommand: string = frontCommentChar + ' ' + command
+  return commentedCommand
+}
+
+export function quickPicker(editor: any, howdoiResultObj: HowdoiObj, userCommand: string): void {
+  const quickPick = vscode.window.createQuickPick()
+
+  quickPick.items = howdoiResultObj.answer.map((answer: string) => (
+    {label: answer, link: howdoiResultObj.link[howdoiResultObj.answer.indexOf(answer)] }))
+  console.log('quickpick:', quickPick.items)
+
+  quickPick.onDidChangeSelection(([item]: any) => {
+    if (item) {
+      editor.edit((edit: any) => {
+        edit.replace(editor.selection, userCommand + '\n' + item.link + '\n' + item.label)
+      })
+	  quickPick.dispose()
+    }
+  })
+  quickPick.onDidHide(() => quickPick.dispose())
+  quickPick.show()
 }
 
 export function deactivate() {}
--- extension/vscode-howdoi/src/test/suite/extension.test.ts
+++ extension/vscode-howdoi/src/test/suite/extension.test.ts
@@ -1,60 +1,176 @@
 import * as assert from 'assert'
 import * as vscode from 'vscode'
 import * as Mocha from 'mocha'
-import * as myExtension from '../../extension'
+import * as myExt from '../../extension'
 
 suite('Extension Test Suite', () => {
   vscode.window.showInformationMessage('Start all tests.')
-  suite('removeHowdoiPrefix test', () => {
-    test('test 1', function () {
-      assert.equal(myExtension.removeHowdoiPrefix('howdoi query'), 'query')
-    })
-    test('test 2', function () {
-      assert.equal(myExtension.removeHowdoiPrefix(' howdoi query '), 'query')
-    })
-    test('test 3', function () {
-      assert.equal(myExtension.removeHowdoiPrefix('query'), 'query')
-    })
-    test('test 4', function () {
-      assert.equal(myExtension.removeHowdoiPrefix(' query '), 'query')
-    })
+  
+  // describe('activate-> #activate', function () {
 
-  })
-  suite('modifyCommentedText test', () => {
+  // })
 
-    test('test 1', function () {
-      // null
-      assert.equal(myExtension.modifyCommentedText('howdoi query'), null)
+  suite('Find comment regex in string -> #findCommentChar', function () {
+    test('String w/o comment regex', function () {
+      // null example
+      assert.equal(myExt.findCommentChar('howdoi query'), null)
     })
-    test('test 2', function () {
+    test('Comment regex test: //', function () {
       // JS, TS, C/ C++/ C#, Java, GO, Rust, Scala, Swift, J#, Dlang single line comment
-      assert.deepEqual(myExtension.modifyCommentedText('// howdoi query'), ['howdoi query', '//', ''])
-      assert.deepEqual(myExtension.modifyCommentedText('//howdoi query'), ['howdoi query', '//', ''])
+      assert.deepEqual(myExt.findCommentChar('// howdoi query'), { frontComment: '//', endComment: '' })
+      // W/o whitespace after comment regex
+      assert.deepEqual(myExt.findCommentChar('//howdoi query'), { frontComment: '//', endComment: '' })
     })
-    test('test 3', function () {
+    test('Comment regex test: #', function () {
       // Python, Ruby, powershell, Julia, R, prolog, Crystal, Dockerfile, Diff single line comment
-      assert.deepEqual(myExtension.modifyCommentedText('# howdoi query'), ['howdoi query', '#', ''] )
-      assert.deepEqual(myExtension.modifyCommentedText('#howdoi query'), ['howdoi query', '#', ''] )
+      assert.deepEqual(myExt.findCommentChar('# howdoi query'), { frontComment: '#', endComment: '' })
+      // W/o whitespace 
+      assert.deepEqual(myExt.findCommentChar('#howdoi query'), { frontComment: '#', endComment: '' })
     })
-    test('test 4', function () {
+    test('Comment regex test: /* */', function () {
       // C++, CSS single line comment 
-      assert.deepEqual(myExtension.modifyCommentedText('/* howdoi query */'), ['howdoi query', '/*', '*/'] )
+      assert.deepEqual(myExt.findCommentChar('/* howdoi query */'), { frontComment: '/*', endComment: '*/' })
+      // W/o whitespace
+      assert.deepEqual(myExt.findCommentChar('/*howdoi query*/'), { frontComment: '/*', endComment: '*/' })
     })
-    test('test 5', function () {
+    test('Comment regex test: <!-- -->', function () {
       // HTML, PHP, Markdown, Vue single line comment
-      assert.deepEqual(myExtension.modifyCommentedText('<!-- howdoi query -->'), ['howdoi query', '<!--', '-->'] )
+      assert.deepEqual(myExt.findCommentChar('<!-- howdoi query -->'), { frontComment: '<!--', endComment: '-->' })
+      // W/o whitespace 
+      assert.deepEqual(myExt.findCommentChar('<!--howdoi query-->'), { frontComment: '<!--', endComment: '-->' })
     })
-    test('test 6', function () {
+    test('Comment regex test: --', function () {
       // SQL, Haskell single line comment
-      assert.deepEqual(myExtension.modifyCommentedText('-- howdoi query'), ['howdoi query', '--', ''] )
+      assert.deepEqual(myExt.findCommentChar('-- howdoi query'), { frontComment: '--', endComment: '' })
+      // W/o whitespace 
+      assert.deepEqual(myExt.findCommentChar('--howdoi query'), { frontComment: '--', endComment: '' })
     })
-    test('test 7', function () {
+    test('Comment regex test: %', function () {
       // LaTex single line comment
-      assert.deepEqual(myExtension.modifyCommentedText('% howdoi query'), ['howdoi query', '%', ''] )
+      assert.deepEqual(myExt.findCommentChar('% howdoi query'), { frontComment: '%', endComment: '' })
+      // W/o whitespace
+      assert.deepEqual(myExt.findCommentChar('%howdoi query'), { frontComment: '%', endComment: '' })
     })
-    test('test 8', function () {
+    test('Comment regex test: ;', function () {
       // clojure single line comment
-      assert.deepEqual(myExtension.modifyCommentedText('; howdoi query'), ['howdoi query', ';', ''] ) 
-    }) 
+      assert.deepEqual(myExt.findCommentChar('; howdoi query'), { frontComment: ';', endComment: '' })
+      // W/o whitespace
+      assert.deepEqual(myExt.findCommentChar(';howdoi query'), { frontComment: ';', endComment: '' })
+    })
   })
+  
+  suite('Removal of comment character from user command -> #removeCommentChar', function () {
+    test('Removal of comment char: //', function () {
+      const commentChar = {frontComment: '//', endComment: ''}
+      // With space
+      assert.deepEqual(myExt.removeCommentChar('// howdoi query', commentChar), 'howdoi query')
+      // W/o whitespace after comment regex
+      assert.deepEqual(myExt.removeCommentChar('//howdoi query', commentChar), 'howdoi query')
+    })
+    test('Removal of comment char: #', function () {
+      const commentChar = { frontComment: '#', endComment: '' }
+      // With space
+      assert.deepEqual(myExt.removeCommentChar('# howdoi query', commentChar), 'howdoi query')
+      // W/o whitespace 
+      assert.deepEqual(myExt.removeCommentChar('#howdoi query', commentChar), 'howdoi query')
+    })
+    test('Removal of comment char: /* */', function () {
+      const commentChar = { frontComment: '/*', endComment: '*/' }
+      // With space
+      assert.deepEqual(myExt.removeCommentChar('/* howdoi query */', commentChar), 'howdoi query')
+      // W/o whitespace
+      assert.deepEqual(myExt.removeCommentChar('/*howdoi query*/', commentChar), 'howdoi query')
+    })
+    test('Removal of comment char: <!-- -->', function () {
+      const commentChar = { frontComment: '<!--', endComment: '-->' }
+      // With space
+      assert.deepEqual(myExt.removeCommentChar('<!-- howdoi query -->', commentChar), 'howdoi query')
+      // W/o whitespace 
+      assert.deepEqual(myExt.removeCommentChar('<!--howdoi query-->', commentChar), 'howdoi query')
+    })
+    test('Removal of comment char: --', function () {
+      const commentChar = { frontComment: '--', endComment: '' }
+      // With space
+      assert.deepEqual(myExt.removeCommentChar('-- howdoi query', commentChar), 'howdoi query')
+      // W/o whitespace 
+      assert.deepEqual(myExt.removeCommentChar('--howdoi query', commentChar), 'howdoi query')
+    })
+    test('Removal of comment char: %', function () {
+      const commentChar = { frontComment: '%', endComment: '' }
+      // LaTex single line comment
+      assert.deepEqual(myExt.removeCommentChar('% howdoi query', commentChar), 'howdoi query')
+      // W/o whitespace
+      assert.deepEqual(myExt.removeCommentChar('%howdoi query', commentChar), 'howdoi query')
+    })
+    test('Removal of comment char: ;', function () {
+      const commentChar = { frontComment: ';', endComment: '' }
+      // clojure single line comment
+      assert.deepEqual(myExt.removeCommentChar('; howdoi query', commentChar), 'howdoi query')
+      // W/o whitespace
+      assert.deepEqual(myExt.removeCommentChar(';howdoi query', commentChar), 'howdoi query')
+    })
+  })
+
+  // describe('retrieveHowdoiOutput function-> #retrieveHowdoiOutput', function () {
+  //   it('', function () {
+  //   })
+  // })
+
+  suite('Removal of howdoi prefix test -> #removeHowdoiPrefix', function () {
+    test('Normal Query', function () {
+      // Normal Query
+      assert.equal(myExt.removeHowdoiPrefix('howdoi query'), 'query')
+    })
+    test('Query with whitespace', function () {
+      // Query with whitespace
+      assert.equal(myExt.removeHowdoiPrefix(' howdoi query '), 'query')
+    })
+    test('Query without howdoi prefix', function () {
+      // Query without howdoi prefix
+      assert.equal(myExt.removeHowdoiPrefix('query'), 'query')
+    })
+    test('Query without howdoi prefix and whitespace', function () {
+      // Query without howdoi prefix and whitespace
+      assert.equal(myExt.removeHowdoiPrefix(' query '), 'query')
+    })
+  })
+
+  // describe('HowdoiObj interface creation test -> #createHowdoiObj', function () {
+  //   it('', function () {
+  //   })
+  // })
+
+  suite('Add comment character to a string -> #addComment', function () {
+    test('Add comment with front/back chars to string', function () {
+      // Add comment: '/*' '*/'
+      const commentChar1 = { frontComment: '/*', endComment: '*/' }
+      assert.equal(myExt.addComment('howdoi query', commentChar1), '/* howdoi query */')
+      // Add comment with '<!--' '-->' 
+      const commentChar2 = { frontComment: '<!--', endComment: '-->' }
+      assert.equal(myExt.addComment('howdoi query', commentChar2), '<!-- howdoi query -->')
+    })
+    test('Add comment with front char to string', function () {
+      // Add comment: #
+      const commentChar1 = { frontComment: '#', endComment: '' }
+      assert.equal(myExt.addComment('howdoi query', commentChar1), '# howdoi query')
+      // Add comment: ;
+      const commentChar2 = { frontComment: ';', endComment: '' }
+      assert.equal(myExt.addComment('howdoi query', commentChar2), '; howdoi query')
+      // Add comment: %
+      const commentChar3 = { frontComment: '%', endComment: '' }
+      // LaTex single line comment
+      assert.equal(myExt.addComment('howdoi query', commentChar3), '% howdoi query')
+      // Add coment: -- 
+      const commentChar4 = { frontComment: '--', endComment: '' }
+      assert.equal(myExt.addComment('howdoi query', commentChar4), '-- howdoi query')
+      // Add comment: //
+      const commentChar5 = { frontComment: '//', endComment: '' }
+      assert.equal(myExt.addComment('howdoi query', commentChar5), '// howdoi query')
+    })
+  })
+
+  // describe('quickpicker-> #quickpicker', function () {
+  //   it('', function () {
+  //   })
+  // })
 })
