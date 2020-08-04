26e8f606db14b623f69136c08b671942a6dfc320
Diana Arreola <arreoladiana11@gmail.com>
2020-08-04 14:25:31 -0700

    Integrated plugin code into vscode ext

--- extension/code-editor-integration/src/plugin.ts
+++ extension/code-editor-integration/src/plugin.ts
@@ -1,5 +1,6 @@
 'use strict'
 import * as cp from 'child_process'
+import {once} from 'events'
 
 const HOWDOI_PREFIX = 'howdoi'
 
@@ -15,36 +16,47 @@ interface JSONObj {
   position: string
 }
 
-interface CallBack {
-  (howdoiJSON: JSONObj[]): HowdoiObj
-}
-
 interface CommentChars {
   frontComment: string,
   endComment: string
 }
 
-export function runHowdoi(userCommand: string): void {
+export async function runHowdoi(userCommand: string): Promise<HowdoiObj> {
+  
+  let commentChar: CommentChars
+  // check if query is enclosed by a single line comment and return commentChar
+  try {
+    commentChar = findCommentChar(userCommand)
+  }catch (e) {
+    throw new ReferenceError('Invalid line comment. Please use single line comment for howdoi.')
+  }
+  
+  const commandWithoutComment: string = removeCommentChar(userCommand, commentChar)
+  let commandWithoutPrefix: string
 
-  // retrieve single line comment regexes
-  const commentChar: CommentChars|null = findCommentChar(userCommand)
-    
-  if (commentChar !== null) {
-    // comment removed from usercommand
-    const commandWithoutComment: string = removeCommentChar(userCommand, commentChar)
-    
-    const callbackFunc: CallBack = function(howdoiJSON: JSONObj[]): HowdoiObj{
-      return createHowdoiObj(howdoiJSON, userCommand, commentChar)
-    }
+  // check if howdoi prefix is present and remove it
+  try {
+    commandWithoutPrefix = removeHowdoiPrefix(commandWithoutComment)
+  }catch (e) {
+    throw new SyntaxError('Place "howdoi" in front of query')
+  }
 
-    // retrieve answer for howodoi query
-    retrieveHowdoiOutput(commandWithoutComment, callbackFunc)
-  }   
+  let parsedJson: JSONObj[] 
+  // check if howdoi output is valid and save the JSON Obj
+  try {
+    parsedJson = await retrieveHowdoiOutput(commandWithoutPrefix)
+  }catch (e) {
+    throw new Error('Invalid json object or no json object returned')
+  }
+  
+  const howdoiResultObj = createHowdoiObj(parsedJson, userCommand, commentChar)
+  return howdoiResultObj
 }
 
-export function findCommentChar(userCommand: string): CommentChars|null { 
+export function findCommentChar(userCommand: string): CommentChars { 
   /* This function finds the comment regex, removes it from the string and returns a
-   CommentChars interface with the beginning comment regex and ending comment regex */
+   CommentChars interface with the beginning comment regex and ending comment regex or returns
+   null if there is no comment regex or an invalid comment regex*/
   const frontCommentRegex =  /^[!@#<>/;%*(+=._-]+/
   const endCommentRegex = /[!@#<>/%*+=._-]+$/
   let frontCommentChar: string
@@ -62,14 +74,6 @@ export function findCommentChar(userCommand: string): CommentChars|null {
     }
     return userCommandWithoutComment
   }
-  else if(endMatchRegex){
-    endCommentChar = endMatchRegex.join()
-    userCommandWithoutComment = {
-      frontComment: '',
-      endComment: endCommentChar
-    }
-    return userCommandWithoutComment
-  }
   else if(initialMatchRegex){
     frontCommentChar = initialMatchRegex.join()
     userCommandWithoutComment = {
@@ -79,7 +83,7 @@ export function findCommentChar(userCommand: string): CommentChars|null {
     return userCommandWithoutComment
   }
   else {
-    return null
+    throw Error('Invalid line comment. Please use single line comment for howdoi.')
   }
 }
 
@@ -95,35 +99,42 @@ export function removeCommentChar(userCommand: string, commentChar: CommentChars
   return userCommand.replace(endCommentChar, '').trim()
 }
 
-export async function retrieveHowdoiOutput(command: string, callbackFunc: CallBack): Promise<void> {
+export async function retrieveHowdoiOutput(command: string): Promise<JSONObj[]> {
   // spawns an external application in a new process to run the howdoi query and retreives
   // the howdoi query answer 
-  const commandWithoutPrefix = removeHowdoiPrefix(command)
-  const process = cp.spawn(HOWDOI_PREFIX, [commandWithoutPrefix, '-n3', '-j'])
-  let howdoiJSON: JSONObj[]
-
+  const process = cp.spawn(HOWDOI_PREFIX, [command, '-n3', '-j'])
+  let howdoiJSON: JSONObj[] = [{ answer: '', link: '', position: ''}]
+  
   process.stdout.on('data', (data: string) => {
     howdoiJSON = JSON.parse(data)
   })
 
-  process.stderr.on('data', (data: Buffer) => {
-    console.log(`stderr: ${data}`)
+  process.stderr.on('dataErr', (dataErr: Buffer) => {
+    console.log(`stderr: ${dataErr}`)
   })
     
   process.on('error', (error: Error) => {
     console.log(`error: ${error.message}`)
   })
     
-  process.on('close', (code:number) => {
+  process.on('close', (code: number) => {
     console.log(`child process exited with code ${code}`)
-    return callbackFunc(howdoiJSON)
-  })
+  }) 
+  
+  // Wait for the child process to exit
+  let endProcess 
+  try {
+    endProcess = await once(process, 'close')
+  } catch(e) {
+    throw Error('Invalid json object or no json object returned')
+  }
+  return howdoiJSON
 }
 
 export function removeHowdoiPrefix(command: string): string {
   // removes the prefix `howdoi` from the string
   if (!command.trim().startsWith(HOWDOI_PREFIX)) {
-    return command.trim()
+    throw Error('Place "howdoi" in front of query')
   }
   return command.replace(HOWDOI_PREFIX, '').trim()
 }
@@ -136,7 +147,6 @@ export function createHowdoiObj(parsedJson: JSONObj[], userCommand: string, comm
     howdoiObj.answer.push(parsedJson[i].answer.trim())
     howdoiObj.link.push(addComment(parsedJson[i].link.trim(), commentChar))
   }
-
   return howdoiObj
 }
 
--- extension/vscode-howdoi/src/extension.ts
+++ extension/vscode-howdoi/src/extension.ts
@@ -1,32 +1,10 @@
 import * as vscode from 'vscode'
-import * as cp from 'child_process'
+import * as plugin from './code-editor-integration/src/plugin'
 
-const HOWDOI_PREFIX = 'howdoi'
-
-interface HowdoiObj {
-    question: string
-    answer: string[]
-    link: string[]
-}
-
-interface JSONObj {
-  answer: string,
-  link: string,
-  position: string
-}
-
-interface CallBack {
-  (howdoiOutput: JSONObj[]): void
-}
-
-interface CommentChars {
-  frontComment: string,
-  endComment: string
-}
 
 export function activate(context: vscode.ExtensionContext) {
 
-  let disposable = vscode.commands.registerCommand('howdoi.extension', () => {
+  let disposable = vscode.commands.registerCommand('howdoi.extension', async () => {
 
     const editor = vscode.window.activeTextEditor
     if (!editor) {
@@ -35,132 +13,30 @@ export function activate(context: vscode.ExtensionContext) {
     }
 
     const userCommand: string = editor.document.getText(editor.selection).trim()
-    
-    // retrieve single line comment regexes
-    const commentChar: CommentChars|null = findCommentChar(userCommand)
-
-    if (commentChar !== null) {
-      // comment removed from usercommand
-      const commandWithoutComment: string = removeCommentChar(userCommand, commentChar)
-
-      // create callback function
-      const callbackFunc: CallBack = function(howdoiJSON: JSONObj[]): void{
-        let howdoiResultObj = createHowdoiObj(howdoiJSON, userCommand, commentChar)
-        
-        quickPicker(editor, howdoiResultObj, userCommand)
+    let howdoiResultObj 
+  
+    try {
+      howdoiResultObj = await plugin.runHowdoi(userCommand)
+    } catch (e) {
+      if (e instanceof ReferenceError) {
+        vscode.window.showInformationMessage('Invalid line comment. Please use single line comment for howdoi.')
+      } else if (e instanceof SyntaxError) {
+        vscode.window.showInformationMessage('Place "howdoi" in front of query')
+      }else if (e instanceof Error) {
+        vscode.window.showInformationMessage('Could not find response for query')
+      } else {
+        vscode.window.showInformationMessage('Error. Try again')
       }
+    } 
 
-      retrieveHowdoiOutput(commandWithoutComment, callbackFunc)
-    }
-    else {
-      vscode.window.showErrorMessage('please use single line comment for howdoi.')
-    }
+    quickPicker(editor, howdoiResultObj, userCommand)
+  
+ 
   })
   context.subscriptions.push(disposable)
 }
 
-export function findCommentChar(userCommand: string): CommentChars|null { 
-  /* This function finds the comment regex, removes it from the string and returns a
-   CommentChars interface with the beginning comment regex and ending comment regex */
-  const frontCommentRegex =  /^[!@#<>/;%*(+=._-]+/
-  const endCommentRegex = /[!@#<>/%*+=._-]+$/
-  let frontCommentChar: string
-  let endCommentChar: string
-  let userCommandWithoutComment: CommentChars
-  const initialMatchRegex: RegExpMatchArray | null = userCommand.match(frontCommentRegex)
-  const endMatchRegex: RegExpMatchArray | null = userCommand.match(endCommentRegex)
-        
-  if (initialMatchRegex && endMatchRegex){
-    frontCommentChar = initialMatchRegex.join()
-    endCommentChar = endMatchRegex.join()
-    userCommandWithoutComment = {
-      frontComment: frontCommentChar,
-      endComment: endCommentChar
-    }
-    return userCommandWithoutComment
-  }
-  else if(initialMatchRegex){
-    frontCommentChar = initialMatchRegex.join()
-    userCommandWithoutComment = {
-      frontComment: frontCommentChar,
-      endComment: ''
-    }
-    return userCommandWithoutComment
-  }
-  else {
-    return null
-  }
-}
-
-export function removeCommentChar(userCommand: string, commentChar: CommentChars): string {
-  // removes single line comment regex from the user command
-  const frontCommentChar: string = commentChar.frontComment
-  const endCommentChar: string = commentChar.endComment
-
-  if (!userCommand.includes(endCommentChar)) {
-    return userCommand.replace(frontCommentChar, '').trim()
-  }
-  userCommand = userCommand.replace(frontCommentChar, '')
-  return userCommand.replace(endCommentChar, '').trim()
-}
-
-export async function retrieveHowdoiOutput(command: string, callbackFunc: CallBack): Promise<void> {
-  
-  const commandWithoutPrefix = removeHowdoiPrefix(command)
-  // HOWDOI_PREFIX, [commandWithoutPrefix, '-n3']
-  const process = cp.spawn(HOWDOI_PREFIX, [commandWithoutPrefix, '-n3', '-j'])
-  
-  let howdoiJSON: JSONObj[]
-
-  process.stdout.on('data', (data: string) => {
-    howdoiJSON = JSON.parse(data)
-  })
-
-  process.stderr.on('data', (data: Buffer) => {
-    console.log(`stderr: ${data}`)
-  })
-
-  process.on('error', (error: Error) => {
-    console.log(`error: ${error.message}`)
-  })
-
-  process.on('close', (code: number) => {
-    console.log(`child process exited with code ${code}`)
-    return callbackFunc(howdoiJSON)
-  })
-}
-
-export function removeHowdoiPrefix(command: string): string {
-  if (!command.trim().startsWith(HOWDOI_PREFIX)) {
-    return command.trim()
-  }
-  return command.replace(HOWDOI_PREFIX, '').trim()
-}
-
-export function createHowdoiObj(parsedJson: JSONObj[], userCommand: string, commentChar: CommentChars): HowdoiObj {
-  // creates a HowdoiObj interface 
-  const howdoiObj: HowdoiObj = {question: userCommand, answer: [], link: []}
-
-  for (let i = 0; i < parsedJson.length; i++) {
-    howdoiObj.answer.push(parsedJson[i].answer.trim())
-    howdoiObj.link.push(addComment(parsedJson[i].link.trim(), commentChar))
-  }
-  return howdoiObj
-}
-
-export function addComment(command: string, commentChar: CommentChars): string {
-  // adds single line comment to string provided
-  const frontCommentChar: string = commentChar.frontComment
-  const endCommentChar: string = commentChar.endComment
-  if (frontCommentChar && (endCommentChar !== '')) {
-    const commentedCommand: string = frontCommentChar + ' ' + command + ' ' + endCommentChar
-    return commentedCommand
-  }
-  const commentedCommand: string = frontCommentChar + ' ' + command
-  return commentedCommand
-}
-
-function quickPicker(editor: any, howdoiResultObj: HowdoiObj, userCommand: string): void {
+function quickPicker(editor: any, howdoiResultObj: any, userCommand: string): void {
   const quickPick = vscode.window.createQuickPick()
 
   quickPick.items = howdoiResultObj.answer.map((answer: string) => (
