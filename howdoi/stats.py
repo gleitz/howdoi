101649d74118cd1340137eefae65d11c9a628904
Eyitayo Ogunbiyi <eyitayoogunbiyi@gmail.com>
2020-08-05 17:34:10 +0100

    refactored stats class

--- howdoi/stats.py
+++ howdoi/stats.py
@@ -12,30 +12,43 @@ DEFAULT_STORE_DIR = appdirs.user_cache_dir('howdoi-stats')
 
 class Keys:
     FIRST_INSTALL_DATE = 'FIRST_INSTALL_DATE'
-
-
-class Store:
-    def __init__(self, store_dir=DEFAULT_STORE_DIR):
-        self._cache = FileSystemCache(store_dir, default_timeout=0)
-        if not self._cache.has(Keys.FIRST_INSTALL_DATE):
-            self._cache.clear()
-            self._cache.set(Keys.FIRST_INSTALL_DATE, datetime.today().strftime("%Y-%m-%d"))
-
-    def __getitem__(self, key):
-        return self._cache.get(key)
-
-    def __setitem__(self, key, item):
-        self._cache.add(key, item)
+    HOUR_OF_DAY_PREFIX = 'STATS_HOUR_OF_DAY'
+    QUERY_COUNT_PREFIX = 'QUERY_COUNT_PREFIX'
 
 
 class Stats:
-    def __init__(self, store):
-        self.store = store
+    def __init__(self, cache):
+        self.DISALLOWED_WORDS = set(['in', 'a', 'an', 'for', 'on'])
+        self.cache = cache
+        if not self.cache.has(Keys.FIRST_INSTALL_DATE):
+            self.cache.clear()
+            self.cache.set(Keys.FIRST_INSTALL_DATE, datetime.today().strftime("%Y-%m-%d"))
 
     def get_days_since_first_install(self):
-        first_install_date = self.store[Keys.FIRST_INSTALL_DATE]
+        first_install_date = self.cache.get(Keys.FIRST_INSTALL_DATE)
         delta = datetime.today() - datetime.strptime(first_install_date, "%Y-%m-%d")
         return delta.days
 
-    def store_query(self, query):
-        pass
+    def increment_key(self, key):
+        self.cache.inc(key)
+
+    def __getitem__(self, key):
+        return self.cache.get(key)
+
+    def process_query_string(self, querystring):
+        if not querystring:
+            return
+        words = querystring.split(" ")
+        for word in words:
+            word = word.lower()
+            if word not in self.DISALLOWED_WORDS:
+                self.increment_key(Keys.QUERY_COUNT_PREFIX+word)
+
+    def store_current_hour_of_day(self):
+        curr_hour_of_day = datetime.now().hour
+        key = Keys.HOUR_OF_DAY_PREFIX + str(curr_hour_of_day)
+        self.increment_key(key)
+
+    def process_args(self, args):
+        self.store_current_hour_of_day()
+        self.process_query_string(args.get('query'))
