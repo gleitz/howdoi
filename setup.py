bd204067ca38a88e2ecdc47f54f8d172a59e81bc
Kimaru thagana <macbuntu@192.168.1.7>
2021-02-27 20:54:44 +0300

    delete all 3.5 references and workflows

--- setup.py
+++ setup.py
@@ -43,7 +43,6 @@ setup(
         "Environment :: Console",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
