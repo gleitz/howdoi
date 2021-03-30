ffde421144d7106932221953be124f0a3acbbff7
Kimaru thagana <macbuntu@192.168.1.7>
2021-03-30 17:39:23 +0300

    pylint updates for errors.py

--- howdoi/errors.py
+++ howdoi/errors.py
@@ -3,12 +3,12 @@ import logging
 
 class GoogleValidationError(Exception):
 
-    logging.exception(f'Google Validation Error{Exception}')
+    logging.exception(f'Google Validation Error{Exception}')  # pylint: disable=Use lazy % formatting in logging functions (logging-fstring-interpolation)
 
 
 class BingValidationError(Exception):
-    logging.exception(f'Bing Validation Error{Exception}')
+    logging.exception(f'Bing Validation Error{Exception}')  # pylint: disable=Use lazy % formatting in logging functions (logging-fstring-interpolation)
 
 
 class DDGValidationError(Exception):
-    logging.exception(f'DDG Validation Error{Exception}')
+    logging.exception(f'DDG Validation Error{Exception}')  #  pylint: disable=Use lazy % formatting in logging functions (logging-fstring-interpolation)
