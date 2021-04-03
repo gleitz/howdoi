ba13d9659084432f5202533d56d78df48b65a892
Kimaru thagana <macbuntu@localhost.localdomain>
2021-04-03 13:02:13 +0300

    add consuructor to class

--- howdoi/errors.py
+++ howdoi/errors.py
@@ -3,12 +3,16 @@ import logging
 
 class GoogleValidationError(Exception):
 
-    logging.exception('Google Validation Error %s ', Exception)
+    def __init__(self):
+        logging.exception('Google Validation Error %s ', Exception)
 
 
 class BingValidationError(Exception):
-    logging.exception('Bing Validation Error %s ', Exception)
+
+    def __init__(self):
+        logging.exception('Bing Validation Error %s ', Exception)
 
 
 class DDGValidationError(Exception):
-    logging.exception('DDG Validation Error %s', Exception)
+    def __init__(self):
+        logging.exception('DDG Validation Error %s', Exception)
